fc585550f2dc9d6528eed3e2afa5be3518e2a0c3
dasa123 <david@sardari.eu>
2014-08-13 19:07:43 +0200

    pure ssl if chosen

--- public/javascripts/leaflet_tutorial.js
+++ public/javascripts/leaflet_tutorial.js
@@ -10,7 +10,7 @@ function initmap() {
 	map = new L.Map('map');
 
 	// create the tile layer with correct attribution
-	var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmUrl = '//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 	var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 	var osm = new L.TileLayer(osmUrl, {
 		minZoom : 8,
