fee675f68fad3186527e04690d3b3e3a84430cb0
dasa123 <david@sardari.eu>
2014-09-06 23:04:39 +0200

    photo zoom and close

--- public/javascripts/photo_user_defined.js
+++ public/javascripts/photo_user_defined.js
@@ -18,12 +18,12 @@ function showPhoto(photo) {
 	photoArray = [];
 	for ( var i = 0; i < tempPhotoArray.length; i++) {
 		var currentPhoto = tempPhotoArray[i];
-		var google_id = '';
+		var google_id = null;
 		if (photo.options.id === currentPhoto.options.id) {
 			google_id = my_google_id;
 		}
 		addMarker(google_id, currentPhoto._latlng.lat,
-				currentPhoto._latlng.lng, 'photo', currentPhoto.options.id, '');
+				currentPhoto._latlng.lng, 'photo', currentPhoto.options.id, null);
 		map.removeLayer(currentPhoto);
 	}
 	expandPhoto();
@@ -32,3 +32,21 @@ function showPhoto(photo) {
 		'photoId' : photo.options.id
 	});
 }
+
+$(document).on('click', '#photo_close', function() {
+	var tempPhotoArray = photoArray;
+	photoArray = [];
+	for ( var i = 0; i < tempPhotoArray.length; i++) {
+		var currentPhoto = tempPhotoArray[i];
+		addMarker(null, currentPhoto._latlng.lat,
+				currentPhoto._latlng.lng, 'photo', currentPhoto.options.id, null);
+		map.removeLayer(currentPhoto);
+	}
+	collapsePhoto();
+});
+
+$(document).on('click', '#photo_zoom', function() {
+	var photo_id = parseInt($('#photo_id').val());
+	var marker = getMarker('photo', photo_id);
+	map.setView(marker._latlng);
+});
